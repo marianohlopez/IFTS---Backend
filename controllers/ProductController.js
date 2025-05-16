ee9866dd2b19cf244b1e22cd363d66d9e9cd58fd
mlopez <ml.3012@gmail.com>
2025-05-16 18:53:32 -0300

    put delete products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,10 +1,11 @@
 import Product from '../classes/Product.js';
 
+const productModel = new Product();
+
 // Funcion para listar productos
 
 export const listProducts = async (req, res) => {
   try {
-    const productModel = new Product();
     const products = await productModel.getAll();
     res.render('productList', { products });
   } catch (err) {
@@ -16,7 +17,6 @@ export const listProducts = async (req, res) => {
 
 export const addProduct = async (req, res) => {
   try {
-    const productModel = new Product();
     const products = await productModel.getAll();
     const { name, stock, price } = req.body;
     const id = products.length ? products[products.length - 1].id + 1 : 1;
@@ -28,3 +28,44 @@ export const addProduct = async (req, res) => {
     res.status(500).send('Error adding product');
   }
 };
+
+// Actualizar un producto existente
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, price, stock } = req.body;
+    const products = await productModel.getAll();
+    const productIndex = products.findIndex(p => p.id === parseInt(id));
+
+    if (productIndex === -1) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+
+    // Actualizar el producto
+    products[productIndex] = { ...products[productIndex], name, price, stock };
+    await productModel.save(products);
+    res.json({ message: 'Producto actualizado', product: products[productIndex] });
+  } catch (err) {
+    console.log(`Error: ${err}`)
+    res.status(500).json({ message: 'Error al actualizar el producto' });
+  }
+};
+
+// Eliminar un producto existente
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await productModel.getAll();
+    const filteredProducts = products.filter(p => p.id !== parseInt(id));
+
+    if (filteredProducts.length === products.length) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+
+    // Guardar el array actualizado
+    await productModel.save(filteredProducts);
+    res.json({ message: 'Producto eliminado' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error al eliminar el producto' });
+  }
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
-import { listProducts, addProduct } from '../controllers/ProductController.js';
+import { listProducts, addProduct, updateProduct, deleteProduct } from '../controllers/ProductController.js';
 
 const router = Router();
 
 // Genero las rutas para los productos utilizando funciones
 // predefinidas en el archivo ProductController.js
 
+// Listar los productos
 router.get('/', listProducts);
+
+// Agregar un producto
 router.post('/add', addProduct);
+
+// Actualizar un producto 
+router.put('/:id', updateProduct);
+
+// Eliminar un producto 
+router.delete('/:id', deleteProduct);
 
 export const productRouter = router;
 
